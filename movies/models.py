8c739cdbdda4de26087aff5886b87475113c22c4
Artem Faradey <artemfap@yandex.ru>
2020-05-27 01:25:06 +0300

    add forms

--- movies/models.py
+++ movies/models.py
@@ -4,77 +4,82 @@ from django.db import models
 
 
 # Create your models here.
+from django.urls import reverse
+
 
 class Category(models.Model):
     """Category"""
-    name = models.CharField("Categoria", max_length=160)
-    description = models.TextField("Opisanie")
-    url = models.SlugField(max_length=160, unique=True)
+    name = models.CharField("Категория", max_length=160, help_text='название категории')
+    description = models.TextField("Описание")
+    url = models.SlugField("Поле slug", max_length=160, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Category"
-        verbose_name_plural = "Categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Actor(models.Model):
     """Actors and directors"""
-    name = models.CharField("Imya", max_length=160)
-    age = models.PositiveSmallIntegerField("Vozrast", default=0)
-    description = models.TextField("Opisanie")
-    image = models.ImageField("Izobrajenie", upload_to="actors/")
+    name = models.CharField("Название", max_length=160)
+    age = models.PositiveSmallIntegerField("Возраст", default=0)
+    description = models.TextField("Описание")
+    image = models.ImageField("Фото", upload_to="actors/")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Actors and directors"
-        verbose_name_plural = "Actors and directors"
+        verbose_name = "Актеры и режиссеры"
+        verbose_name_plural = "Актеры и режиссеры"
 
 
 class Genre(models.Model):
     """Genres"""
-    name = models.CharField("Imya", max_length=160)
-    description = models.TextField("Opisanie")
-    url = models.SlugField(max_length=160, unique=True)
+    name = models.CharField("Название", max_length=160)
+    description = models.TextField("Описание")
+    url = models.SlugField("Поле slug", max_length=160, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Genre"
-        verbose_name_plural = "Genres"
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
 
 
 class Movie(models.Model):
     """Movies"""
-    title = models.CharField("Nazvanie", max_length=100)
-    tagline = models.CharField("Slogan", max_length=100, default="")
-    description = models.TextField("Opisanie")
-    poster = models.ImageField("Poster", upload_to="movies/")
-    year = models.PositiveSmallIntegerField("Date out", default=2020)
-    country = models.CharField("Strana", max_length=50)
-    directors = models.ManyToManyField(Actor, verbose_name="director", related_name="film_director")
-    actors = models.ManyToManyField(Actor, verbose_name="actor", related_name="film_actor")
-    genres = models.ManyToManyField(Genre, verbose_name="Janri")
-    world_premiere = models.DateField("Primiera v mire", default=date.today)
+    title = models.CharField("Название фильма", max_length=100)
+    tagline = models.CharField("Слоган", max_length=100, default="")
+    description = models.TextField("Описание")
+    poster = models.ImageField("Постер", upload_to="movies/")
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2020)
+    country = models.CharField("Страна", max_length=50)
+    directors = models.ManyToManyField(Actor, verbose_name="режиссер", related_name="film_director")
+    actors = models.ManyToManyField(Actor, verbose_name="актёр", related_name="film_actor")
+    genres = models.ManyToManyField(Genre, verbose_name="Жанр")
+    world_premiere = models.DateField("Примьера в мире", default=date.today)
     budget = models.PositiveIntegerField(
-        "Budjet", default=0, help_text="summa v baksah"
+        "Бюджет", default=0, help_text="сумма в долларах"
     )
     fees_in_usa = models.PositiveIntegerField(
-        "Sbori v USA", default=0, help_text="summa v baksah"
+        "Сборы в США", default=0, help_text="сумма в долларах"
     )
     fees_in_world = models.PositiveIntegerField(
-        "Sbori v USA", default=0, help_text="summa v baksah"
+        "Сборы в мире", default=0, help_text="сумма в долларах"
     )
-    category = models.ForeignKey(Category, verbose_name="Categoriya", on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.SET_NULL, null=True)
     url = models.SlugField(max_length=160, unique=True)
     draft = models.BooleanField("Chernovik", default=False)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = "Film"
@@ -84,7 +89,7 @@ class Movie(models.Model):
 class MovieShots(models.Model):
     """Shots from film"""
     title = models.CharField('Zagolovok', max_length=100)
-    description = models.TextField("Opisanie")
+    description = models.TextField("Описание")
     image = models.ImageField('Izobrajenie', upload_to='movie_shots/')
     movie = models.ForeignKey(Movie, verbose_name='Film', on_delete=models.CASCADE)
 
@@ -125,8 +130,8 @@ class Rating(models.Model):
 class Reviews(models.Model):
     """Otzivi"""
     email = models.EmailField()
-    name = models.CharField('Imya', max_length=100)
-    text = models.TextField('Text', max_length=5000)
+    name = models.CharField('Название', max_length=100)
+    text = models.TextField('Текст', max_length=5000)
     parent = models.ForeignKey('self', verbose_name='Roditel', on_delete=models.SET_NULL, blank=True, null=True)
     movie = models.ForeignKey(Movie, verbose_name='Film', on_delete=models.CASCADE)
 
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic import ListView, DetailView
+from django.views.generic.base import View
+
+from movies.models import Movie
+from .forms import ReviewForm
+
+class MoviesView(ListView):
+    """List movies"""
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+
+
+class MovieDetailView(DetailView):
+    """Detail View"""
+    model = Movie
+    slug_field = "url"
+
+class AddReview(View):
+    """Reviews"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
