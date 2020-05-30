bfde52ed64fadb3214f70221254806daef31e84b
Artem Faradey <artemfap@yandex.ru>
2020-05-31 00:57:18 +0300

    fix tempalte v.2.03

--- movies/admin.py
+++ movies/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 from .models import Category, Actor, Genre, Movie, MovieShots, RatingStar, Rating, Reviews
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ('name',)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_display_links = ('title',)
+    list_filter = ('category', 'year')
+    search_fields = ('title', 'category__name')
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'parent', 'movie', 'id')
+    readonly_fields = ('name', 'email', 'text', 'movie', 'parent')
+
+
+
 # Register your models here.site.
-admin.site.register(Category)
-admin.site.register(Actor)
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(MovieShots)
-admin.site.register(RatingStar)
-admin.site.register(Rating)
-admin.site.register(Reviews)
+# admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Actor)
+# admin.site.register(Genre)
+# admin.site.register(Movie)
+# admin.site.register(MovieShots)
+# admin.site.register(RatingStar)
+# admin.site.register(Rating)
+# admin.site.register(Reviews)
